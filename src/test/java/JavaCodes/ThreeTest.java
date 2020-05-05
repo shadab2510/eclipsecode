344a2e94eb164a732b96d8999d0e09004a9f9ae0
shadab <shadab.anwar@traveltriangle.com>
2020-05-06 00:54:22 +0530

    Add new class ThreeTest

---
+++ src/test/java/JavaCodes/ThreeTest.java
@@ -0,0 +1,22 @@
+package JavaCodes;
+
+import org.testng.annotations.Test;
+
+public class ThreeTest {
+	
+	@Test
+	public void ThreeTest()
+	{
+		System.out.println("Three test");
+	}
+
+	
+	
+
+
+@Test
+public void ThreeNew()
+{
+	System.out.println("Three new");
+}
+}
